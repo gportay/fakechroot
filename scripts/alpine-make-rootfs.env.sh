dd57152818adc23f8ac8d5f5b30509fe1d39f8b5
Gaël PORTAY <gael.portay@collabora.com>
2020-09-28 13:39:27 -0400

    Add the replacement executable for busybox
    Note: The option --argv0 /bin/busybox sets the symlink to /bin/busybox
    instead of its full path (i.e. /path/to/chrooted/dir/bin/busybox).

--- scripts/alpine-make-rootfs.env.sh
+++ scripts/alpine-make-rootfs.env.sh
@@ -9,6 +9,9 @@ fi
 # Set a list of command substitutions needed by alpine-make-rootfs
 fakechroot_alpine_make_rootfs_env_cmd_subst="@MOUNT@=/bin/true
 @UMOUNT@=/bin/true
+/bin/busybox=${fakechroot_bindir:-@bindir@}/busybox.fakechroot
+/bin/bbsuid=${fakechroot_bindir:-@bindir@}/busybox.fakechroot
+/bin/busybox-extras=${fakechroot_bindir:-@bindir@}/busybox.fakechroot
 @CHROOT@=${fakechroot_bindir:-@sbindir@}/chroot.fakechroot
 @LDD@=${fakechroot_bindir:-@bindir@}/ldd.fakechroot"
 
@@ -16,7 +19,7 @@ FAKECHROOT_CMD_SUBST="${FAKECHROOT_CMD_SUBST:+$FAKECHROOT_CMD_SUBST:}`echo \"$fa
 export FAKECHROOT_CMD_SUBST
 
 # Set the default list of directories excluded from being chrooted
-FAKECHROOT_EXCLUDE_PATH="${FAKECHROOT_EXCLUDE_PATH:-/dev:/proc:/sys}"
+FAKECHROOT_EXCLUDE_PATH="${FAKECHROOT_EXCLUDE_PATH:-/dev:/proc:/sys:/bin/sh}"
 export FAKECHROOT_EXCLUDE_PATH
 
 # Set the default list of symlinks excluded from being chrooted
---
+++ scripts/busybox.fakechroot.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# busybox
+#
+# Replacement for busybox command which calls the program from the chroot'ed
+# environment.
+#
+# (c) 2020 Gaël PORTAY <gael.portay@gmail.com>, LGPL
+
+# The script is called:
+# /usr/bin/busybox.fakechroot --install -s
+# export FAKECHROOT="true"
+# export FAKECHROOT_BASE_ORIG="/path/to/chrooted/dir"
+# export FAKECHROOT_CMD_ORIG="/bin/busybox"
+# export FAKECHROOT_CMD_SUBST="/usr/sbin/mount=/bin/true:/usr/sbin/umount=/bin/true:/bin/busybox=/usr/bin/busybox.fakechroot:/usr/sbin/chroot=/usr/bin/chroot.fakechroot:/usr/bin/ldd=/usr/bin/ldd.fakechroot:"
+# export FAKECHROOT_EXCLUDE_PATH="/dev:/proc:/sys:/bin/sh"
+# export FAKECHROOT_INCLUDE_PATH="/etc/shadow"
+# export FAKECHROOT_VERSION="2.20.2"
+# export FAKED_MODE="unknown-is-root"
+# export FAKEROOTKEY="123456789"
+# export LD_LIBRARY_PATH="/usr/lib/libfakeroot/fakechroot:/usr/lib/libfakeroot:/usr/lib64/libfakeroot:/usr/lib32/libfakeroot:/path/to/chrooted/dir/usr/lib:/path/to/chrooted/dir/lib"
+# export LD_PRELOAD="libfakechroot.so:libfakeroot.so"
+# export OLDPWD
+# export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+# export PWD="/path/to/chrooted/dir"
+# export SHLVL="2"
+
+# TODO: remove $FAKECHROOT_BASE_ORIG/bin
+LD_LIBRARY_PATH= \
+LD_PRELOAD= \
+"$FAKECHROOT_BASE_ORIG/lib/ld-musl-x86_64.so.1" --argv0 "$FAKECHROOT_CMD_ORIG" \
+"$FAKECHROOT_BASE_ORIG$FAKECHROOT_CMD_ORIG" "$@" \
+"$FAKECHROOT_BASE_ORIG/bin"
